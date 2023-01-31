3d2a895b9bbe7dc00e9c5a4519d37fabdfd41747
Mr.76 <bartmoss77@tutanota.com>
2023-01-31 11:41:42 -0300

    build with cmake

---
+++ project_files/src/main/main.c
@@ -0,0 +1,14 @@
+#include "../readDirs/readAllDirs.h"
+#include "../killPart/kill.h"
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+  if (argc != 3) {
+    printf("Usage: %s <program name> and flag <-NP> or <-P>\n", argv[0]);
+    return 1;
+  }
+  findProcess(argv[1], argv[2]);
+  printf("2nd part\n");
+  killProgram();
+  return 0;
+}
