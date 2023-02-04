924688af592c3f2c2b69220a7a6c20bb8713b6cc
Mr.76 <bartmoss77@tutanota.com>
2023-02-04 13:36:00 -0300

    added now possibility to use sigkill

--- project_files/src/killPart/kill.c
+++ project_files/src/killPart/kill.c
@@ -4,8 +4,22 @@
 void killProgram() {
   char input[100];
   int pid;
-  printf("input the number of the process u want to terminate\n");
+  int sig;
+  printf("Input the number of the process u want to terminate\n");
   fgets(input, sizeof(input), stdin);
   sscanf(input, "%d", &pid);
-  kill(pid, SIGTERM);
+
+  printf("Choose\n1- sigterm\n2- sigkill\nAdvised to use sigterm\n");
+
+  fgets(input, sizeof(input), stdin);
+  sscanf(input, "%d", &sig);
+
+  printf("Ending process %d\n", pid);
+  if (sig == 1) {
+    kill(pid, SIGTERM);
+  } else if (sig == 2) {
+    kill(pid, SIGKILL);
+  }else{
+	  printf("Nothing selected exiting.....\n");
+  }
 }
